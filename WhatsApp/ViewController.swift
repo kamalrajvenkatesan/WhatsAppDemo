0693a2c5499eea390d0f2ba2207595b94651fc96
kamalraj venkatesan <kamalraj.venkatesan@gyanmatrix.com>
2018-09-19 19:52:58 +0530

    listing dummy data

--- WhatsApp/ViewController.swift
+++ WhatsApp/ViewController.swift
@@ -9,16 +9,17 @@
 import UIKit
 import Shared
 
-class ViewController: UIViewController {
+class ViewController: BaseTableViewController<ChatListTableViewCell, ChatList> {
+
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    items = [
+      ChatList(data: "Kamal"),
+      ChatList(data: "Raj"),
+      ChatList(data: "Manjula")
+    ]
   }
 
 
