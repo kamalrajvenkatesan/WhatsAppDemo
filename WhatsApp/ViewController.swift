44216a832d4357e952d9c24ca00272bba9d55ab7
kamalraj venkatesan <kamalraj.venkatesan@gyanmatrix.com>
2018-09-19 21:33:45 +0530

    ViewController: Listing chat

--- WhatsApp/ViewController.swift
+++ WhatsApp/ViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 import Shared
 
-class ViewController: BaseTableViewController<ChatListTableViewCell, ChatList> {
+class ViewController: BaseTableViewController<ChatListTableViewCell, Chat> {
 
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    items = [
-      ChatList(data: "Kamal"),
-      ChatList(data: "Raj"),
-      ChatList(data: "Manjula")
-    ]
+    items = loadJson(type: [Chat].self, fileName: "Chat") ?? []
   }
 
+
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 70
+  }
 
 }
 
