051b5e5b9fa85222f3efe3b6b510e3d3311fb719
kamalraj venkatesan <kamalraj.venkatesan@gyanmatrix.com>
2018-09-20 08:32:41 +0530

    added chat module

---
+++ Chat/TextCell.swift
@@ -0,0 +1,25 @@
+//
+//  TextCell.swift
+//  Chat
+//
+//  Created by kamalraj venkatesan on 20/09/18.
+//  Copyright © 2018 Kamalraj. All rights reserved.
+//
+
+import UIKit
+import Shared
+
+class TextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
