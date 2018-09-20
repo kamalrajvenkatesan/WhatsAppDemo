63446ecd4eb24524f15508d958ab571b70b2d469
kamalraj venkatesan <kamalraj.venkatesan@gyanmatrix.com>
2018-09-20 09:19:22 +0530

    Cell Generic config done

--- Shared/Shared/Cell Config/CellConfigurator.swift
+++ Shared/Shared/Cell Config/CellConfigurator.swift
@@ -8,7 +8,29 @@
 
 import Foundation
 
-protocol ConfigurableCell {
+public protocol ConfigurableCell {
   associatedtype U // Data type
   func configure(data: U)
 }
+
+public protocol CellConfigurator {
+  static var reuseId: String { get }
+  func configure(cell: UIView)
+}
+
+
+public class TableCellConfigurator<T: ConfigurableCell, U>: CellConfigurator where T.U == U, T: UITableViewCell {
+
+  public static var reuseId: String { return String(describing: T.self) }
+
+  public let item: U
+
+  public init(item: U) {
+    self.item = item
+  }
+
+  public func configure(cell: UIView) {
+    (cell as! T).configure(data: item)
+  }
+}
+
