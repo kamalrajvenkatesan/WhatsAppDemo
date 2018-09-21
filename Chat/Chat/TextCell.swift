78dbd14a24d3f16d4912491a4851026d19834eef
kamalraj venkatesan <kamalraj.venkatesan@gyanmatrix.com>
2018-09-21 11:54:09 +0530

    Chat layout done

--- Chat/Chat/TextCell.swift
+++ Chat/Chat/TextCell.swift
@@ -35,6 +35,9 @@ class TextCell: UITableViewCell, ConfigurableCell {
     return label
   }()
 
+  var leftConstraint: NSLayoutConstraint?
+  var trailingConstraint: NSLayoutConstraint?
+
   override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
 
@@ -46,7 +49,16 @@ class TextCell: UITableViewCell, ConfigurableCell {
     self.containerView.addSubview(messageLabel)
     self.containerView.addSubview(timeLabel)
 
-    containerView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 60, paddingBottom: 10, paddingRight: 10, width: 0, height: 0, enableInsets: false)
+    // Constraint
+    containerView.translatesAutoresizingMaskIntoConstraints = false
+    leftConstraint = containerView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10)
+    leftConstraint?.isActive = true
+    trailingConstraint = containerView.rightAnchor.constraint(equalTo: rightAnchor, constant: -10)
+    trailingConstraint?.isActive = true
+
+    containerView.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+    containerView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+
 
     messageLabel.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: nil, right: containerView.rightAnchor, paddingTop: 5, paddingLeft: 5, paddingBottom: 0, paddingRight: 5, width: 0, height: 0, enableInsets: false)
 
@@ -63,5 +75,17 @@ class TextCell: UITableViewCell, ConfigurableCell {
     self.timeLabel.text = "Today"
 
     self.containerView.backgroundColor = (data.isSent) ? #colorLiteral(red: 0.8270000219, green: 0.976000011, blue: 0.7179999948, alpha: 1) : .white
+
+    self.move(isSent: data.isSent)
+  }
+
+  func move(isSent: Bool) {
+    self.leftConstraint?.isActive = false
+    leftConstraint = containerView.leftAnchor.constraint(equalTo: leftAnchor, constant: (isSent ? 60 : 10))
+    leftConstraint?.isActive = true
+
+    self.trailingConstraint?.isActive = false
+    trailingConstraint = containerView.rightAnchor.constraint(equalTo: rightAnchor, constant: (isSent ? -10 : -80) )
+    trailingConstraint?.isActive = true
   }
 }
