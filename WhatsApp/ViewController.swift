d2b513e1a3dcb31679a0ddba99c28e1cd0769795
kamalraj venkatesan <kamalraj.venkatesan@gyanmatrix.com>
2018-09-21 13:02:12 +0530

    Fetching contacts done

--- WhatsApp/ViewController.swift
+++ WhatsApp/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Shared
 import Chat
 import RealmSwift
+import Contacts
 
 class ViewController: BaseTableViewController<ChatListTableViewCell, ChatListModel> {
 
@@ -40,6 +41,8 @@ class ViewController: BaseTableViewController<ChatListTableViewCell, ChatListMod
 
     items = Array(try! Realm().objects(ChatListModel.self).sorted(byKeyPath: "recentInterationTime", ascending: true))
 
+    self.getContactsFromAddressBook()
+
     }
 
   override func viewDidAppear(_ animated: Bool) {
@@ -47,6 +50,54 @@ class ViewController: BaseTableViewController<ChatListTableViewCell, ChatListMod
 
     self.navigationController?.navigationBar.prefersLargeTitles = true
   }
+
+  private func getContactsFromAddressBook() {
+    let status = CNContactStore.authorizationStatus(for: .contacts)
+    if status == .denied || status == .restricted {
+      presentSettingsActionSheet()
+      return
+    }
+
+    // open it
+
+    let store = CNContactStore()
+    store.requestAccess(for: .contacts) { granted, error in
+      guard granted else {
+        DispatchQueue.main.async {
+          self.presentSettingsActionSheet()
+        }
+        return
+      }
+
+      // get the contacts
+
+      var contacts = [CNContact]()
+      let request = CNContactFetchRequest(keysToFetch: [CNContactIdentifierKey as NSString, CNContactFormatter.descriptorForRequiredKeys(for: .fullName), CNContactPhoneNumbersKey as CNKeyDescriptor])
+      do {
+        try store.enumerateContacts(with: request) { contact, stop in
+          contacts.append(contact)
+        }
+      } catch {
+        print(error)
+      }
+
+
+      for contact in contacts {
+        print(contact)
+      }
+    }
+  }
+
+  private func presentSettingsActionSheet() {
+    let alert = UIAlertController(title: "Permission to Contacts", message: "This app needs access to contacts in order to access contacts", preferredStyle: .actionSheet)
+    alert.addAction(UIAlertAction(title: "Go to Settings", style: .default) { _ in
+      let url = URL(string: UIApplicationOpenSettingsURLString)!
+      UIApplication.shared.open(url)
+    })
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    present(alert, animated: true)
+  }
+
 
   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return 70
